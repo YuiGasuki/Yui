3474968ba88481d2d5980c2869cf9baae7c63f5f
Yui_ <140684321+YuiandAzucat@users.noreply.github.com>
2024-04-26 22:16:53 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 document.body.style.backgroundImage="url('"+(Math.floor(Math.random() * (3 - 1 + 1)) + 1)+".jpg')";
+let portrait = 1;
+document.getElementById('body_portrait').onclick = () =>{
+let bodyPortrait = document.getElementById('body_portrait');
+bodyPortrait.style.transition="0.5s"; 
+bodyPortrait.style.width="0px";
+bodyPortrait.style.height="0px";
+setTimeout(function(){
+bodyPortrait.style.width="80px";
+bodyPortrait.style.height="80px";
+bodyPortrait.src="portrait"+portrait+".jpg";
+},500)
+portrait++;
+if(portrait===3){
+portrait = 1;
+}
+}
