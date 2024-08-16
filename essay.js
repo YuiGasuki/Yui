d014161e776e19a4d966a76ba808924666e53a82
Yui <140684321+YuiGasuki@users.noreply.github.com>
2024-08-16 20:07:38 +0800

    Add files via upload

--- essay.js
+++ essay.js
@@ -2,17 +2,31 @@ const params = new URLSearchParams(location.search);
 const textp=params.get('Yui');
 let history = "";
 let ifOnlad = 0;
+window.onpageshow = () =>{
 if(textp!=null&&textp!=""){
 if(params.get('history')!=null&&params.get('history')!=""){
     history+= "-"+params.get('history');
 }
-fetch('1.json',{method: 'GET'}).then(response => response.json()).then(data =>{
+fetch('1.json',{method: 'GET'}).then(response => {
+if (response.ok) {
+getbody();
+return response.json();
+}else{
+window.location.assign("404");
+return "啊，居然加载错误Σ(°Д°;"
+}
+}).then(data =>{
 let datab;
 ifOnlad++;
 if(ifOnlad>=2){
     document.getElementById('first_box').remove();
     document.body.style.overflow="auto";
 }
+if(data[textp-1]===undefined){
+    window.location.assign("404");
+    return "啊，居然加载错误Σ(°Д°;"
+}
+
 for(let onei = 0;onei<data.length;onei++){
 if(data[onei].Yui===textp){
 datab = data[onei];
@@ -22,33 +36,76 @@ break
 document.title="Yui的文章「"+datab.title+"」";
 document.getElementById('title').innerHTML = datab.title;
 document.getElementById('titleimg').src = datab.title_url;
-for(let i=0;i<datab.label.length;i++){
-document.getElementById('label').innerHTML += `<span class="label" onclick="JavaScript:window.open('search.html?q=${datab.label[i]}')">`+datab.label[i]+`</span>`;
-}
+
 document.getElementById('titleimg').onerror = () =>{
 document.getElementById('titleimg').src="none.webp";
 }
+
+let TimeLabel = ``;
+for(let i=0;i<datab.label.length;i++){
+TimeLabel += `<span class="label" onclick="JavaScript:window.open('search.html?q=${datab.label[i]}')">`+datab.label[i]+`</span>`;
+}
+document.getElementById('label').innerHTML =TimeLabel;
+
+
+}).catch(err =>{
+document.querySelectorAll('#first_box p')[0].innerHTML = '加载错误 点击刷新';
+document.querySelectorAll('#first_box p')[0].style.color='red';
+document.querySelectorAll('#first_box p')[0].onclick = () =>{
+location.reload();
+}
 });
+  
+}else{
+    window.location.assign("404");
+}
+}
+
+
+const DarkMode = () =>{
+    if(document.cookie){
+        let data = document.cookie.split(";")[0].split("=")[1];
+        if(data==="true"){
+            document.documentElement.style.setProperty('--nightbackground','rgba(86,86,86,0.85)');
+document.documentElement.style.setProperty('--nightfcolor', '#c5c5c5'); 
+document.documentElement.style.setProperty('--nightbox', '0.5');  
+        }else{
+            document.documentElement.style.setProperty('--nightbackground','rgba(238,238,238,0.85)');
+    document.documentElement.style.setProperty('--nightfcolor', '#000000');
+document.documentElement.style.setProperty('--nightbox', '0');  
+        }
+    }else{
+        document.documentElement.style.setProperty('--nightbackground','rgba(238,238,238,0.85)');
+        document.documentElement.style.setProperty('--nightbox', '0');  
+        document.documentElement.style.setProperty('--nightfcolor', '#000000'); 
+    }
+}
+
+DarkMode();
+
+
+const channel = new BroadcastChannel('Yui_night');
+
+channel.addEventListener('message', (e) => {
+DarkMode();
+})
+
+
+function replacepicture() {
+    
+    if(window.innerHeight <= window.innerWidth){
+        document.body.style.backgroundImage = "url('p_1.webp')";
+    }else{
+        document.body.style.backgroundImage = "url('b_3.webp')";
+    }
+}
+replacepicture();
+window.addEventListener('resize', replacepicture);
+
+
+function getbody(){
 fetch(textp+history+'.txt',{method: 'GET'}).then(response => {
 if (response.ok) {
-
-
-    
-
-
- let gitalk = new Gitalk({
-  clientID: 'Ov23liQRR4VjYR4dXXDv',
-  clientSecret: 'b6e1ceaaea554e362eee96360da7b5f1a4bd0383',
-  repo: 'CommentYui',
-  owner: 'Yuigasuki',
-  admin: ['Yuigasuki'],
-  id: location.pathname+"?Yui="+textp,  
-   distractionFreeMode: false  // Facebook-like distraction free mode
-  
- });
-
-gitalk.render('gitalk-container'); 
-
 return response.text();
 }else{
 window.location.assign("404");
@@ -67,7 +124,18 @@ document.getElementById('body').innerHTML = data;
 document.querySelectorAll('div.code').forEach(el => {
   hljs.highlightElement(el);
 });
+let gitalk = new Gitalk({
+  clientID: 'Ov23liQRR4VjYR4dXXDv',
+  clientSecret: 'b6e1ceaaea554e362eee96360da7b5f1a4bd0383',
+  repo: 'CommentYui',
+  owner: 'Yuigasuki',
+  admin: ['Yuigasuki'],
+  id: location.pathname+"?Yui="+textp,  
+   distractionFreeMode: false  // Facebook-like distraction free mode
+  
+ });
 
+gitalk.render('gitalk-container'); 
 
 
 
@@ -100,52 +168,11 @@ el.childNodes[0].style.marginLeft="0px";
 }
 });
 
+}).catch(err =>{
+document.querySelectorAll('#first_box p')[0].innerHTML = '网络错误 点击刷新';
+document.querySelectorAll('#first_box p')[0].style.color='red';
+document.querySelectorAll('#first_box p')[0].onclick = () =>{
+location.reload();
+}
 });
-
-
-  
-}else{
-    window.location.assign("404");
 }
-
-
-const DarkMode = () =>{
-    if(document.cookie){
-        let data = document.cookie.split(";")[0].split("=")[1];
-        if(data==="true"){
-            document.documentElement.style.setProperty('--nightbackground','rgba(86,86,86,0.8)');
-document.documentElement.style.setProperty('--nightfcolor', '#aaaaaa'); 
-document.documentElement.style.setProperty('--nightbox', '0.5');  
-        }else{
-            document.documentElement.style.setProperty('--nightbackground','rgba(238,238,238,0.8)');
-    document.documentElement.style.setProperty('--nightfcolor', '#606266');
-document.documentElement.style.setProperty('--nightbox', '0');  
-        }
-    }else{
-        document.documentElement.style.setProperty('--nightbackground','rgba(238,238,238,0.8)');
-        document.documentElement.style.setProperty('--nightbox', '0');  
-        document.documentElement.style.setProperty('--nightfcolor', '#606266'); 
-    }
-}
-
-DarkMode();
-
-
-const channel = new BroadcastChannel('Yui_night');
-
-channel.addEventListener('message', (e) => {
-DarkMode();
-})
-
-
-function replacepicture() {
-    
-    if(window.innerHeight <= window.innerWidth){
-        document.body.style.backgroundImage = "url('p_1.webp')";
-    }else{
-        document.body.style.backgroundImage = "url('b_3.webp')";
-    }
-}
-replacepicture();
-window.addEventListener('resize', replacepicture);
-
