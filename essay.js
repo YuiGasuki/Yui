7fc0039724e9eee86145314d5f60fd7147b52775
Yui <140684321+YuiGasuki@users.noreply.github.com>
2024-08-24 02:40:09 +0800

    Add files via upload

--- essay.js
+++ essay.js
@@ -7,18 +7,7 @@ let ifOnlad = 0;
 window.onpageshow = () =>{
 if(textp!=null&&textp!=""){
 
-var gitalk = new Gitalk({
-  clientID: 'Ov23liQRR4VjYR4dXXDv',
-  clientSecret: 'b6e1ceaaea554e362eee96360da7b5f1a4bd0383',
-  repo: 'CommentYui',
-  owner: 'yuigasuki',
-  admin: 'yuigasuki',
-  id: location.pathname+"?Yui="+textp,  
-  distractionFreeMode: true  // Facebook-like distraction free mode
-  
- });
 
-gitalk.render('gitalk-container');
 
 
 if(params.get('history')!=null&&params.get('history')!=""){
