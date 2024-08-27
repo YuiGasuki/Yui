bd2ccda6938911445ab80da01c82355b123f0494
Yui <140684321+YuiGasuki@users.noreply.github.com>
2024-08-27 15:36:17 +0800

    Add files via upload

--- essay.js
+++ essay.js
@@ -79,33 +79,7 @@ location.reload();
 }
 
 
-const DarkMode = () =>{
-    if(document.cookie){
-        let data = document.cookie.split(";")[0].split("=")[1];
-        if(data==="true"){
-            document.documentElement.style.setProperty('--nightbackground','rgba(86,86,86,0.85)');
-document.documentElement.style.setProperty('--nightfcolor', '#c5c5c5'); 
-document.documentElement.style.setProperty('--nightbox', '0.5');  
-        }else{
-            document.documentElement.style.setProperty('--nightbackground','rgba(238,238,238,0.85)');
-    document.documentElement.style.setProperty('--nightfcolor', '#000000');
-document.documentElement.style.setProperty('--nightbox', '0');  
-        }
-    }else{
-        document.documentElement.style.setProperty('--nightbackground','rgba(238,238,238,0.85)');
-        document.documentElement.style.setProperty('--nightbox', '0');  
-        document.documentElement.style.setProperty('--nightfcolor', '#000000'); 
-    }
-}
 
-DarkMode();
-
-
-const channel = new BroadcastChannel('Yui_night');
-
-channel.addEventListener('message', (e) => {
-DarkMode();
-})
 
 
 function replacepicture() {
---
+++ setting.js
@@ -0,0 +1,54 @@
+let ifSetupFamily = true;
+const SetupFamily = document.getElementById('setup_family');
+
+const DarkMode = () =>{
+    if(document.cookie){
+        let data = document.cookie.split(";")[0].split("=")[1];
+        if(data==="true"){
+            document.documentElement.style.setProperty('--nightbackground','rgba(86,86,86,0.85)');
+document.documentElement.style.setProperty('--nightfcolor', '#c5c5c5'); 
+document.documentElement.style.setProperty('--nightbox', '0.5');  
+            return
+     }
+     }
+        document.documentElement.style.setProperty('--nightbackground','rgba(238,238,238,0.85)');
+        document.documentElement.style.setProperty('--nightbox', '0');  
+        document.documentElement.style.setProperty('--nightfcolor', '#000000'); 
+    }
+
+
+DarkMode();
+
+
+const channel = new BroadcastChannel('Yui_night');
+
+channel.addEventListener('message', (e) => {
+DarkMode();
+})
+
+
+
+SetupFamily.onclick = () =>{
+    if(ifSetupFamily){
+        SetupFamily.style.setProperty('--leftData', '-4px');
+        SetupFamily.style.setProperty('--divBackGround', '#aaaaaa');
+        document.documentElement.style.setProperty('--fontSerif', 'fontSerif:GenEiKoburiMin6, Arial Black, Gadget, sans-serif');
+        localStorage.serif = false;
+        ifSetupFamily = false;
+    }else{
+        SetupFamily.style.setProperty('--leftData', '15px');
+        SetupFamily.style.setProperty('--divBackGround', '#FF6699');
+        document.documentElement.style.setProperty('--fontSerif', 'GenEiKoburiMin6, serif, Georgia ,"Palatino Linotype", "Book Antiqua","Times New Roman", Times');
+        localStorage.serif = true;
+        ifSetupFamily = true;
+    }
+}
+
+if(localStorage.serif){
+    if(localStorage.serif==="false"){
+        ifSetupFamily = true;
+    }else{
+        ifSetupFamily = false;
+    }
+    SetupFamily.click();
+}
