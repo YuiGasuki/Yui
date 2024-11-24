afbd14d71411a4d2050c4f26217c8d51fda25ef2
Yui <140684321+YuiGasuki@users.noreply.github.com>
2024-11-24 20:20:02 +0800

    Add files via upload

--- announcement.js
+++ announcement.js
@@ -5,7 +5,7 @@ const announcement = document.getElementById('announcement_p');
 let announcementData = `
 ! 本站是供站长(下文称"我")学习使用，图片素材来自网络，并非商用。无意侵犯贵方的利益请<a href="mailto:2528197707@qq.com">联系我</a>(<a href="mailto:l2528197707@gmail.com">备用邮箱</a>)，我将在48小时内进行删除。
 ☆ 开源地址<a href="https://github.com/YuiGasuki/Yui">Github</a> 本站为静态网站，仅供展示我的学习成果，静态托管在<a href="https://github.com/">Github</a>
-✎.本次更新内容: 修改了部分BUG`;
+✎.本次更新内容: 增加「清空设置数据」选项`;
 let DateTime = new Date();
 let Day = DateTime.getUTCDate();
 let Month = DateTime.getMonth()+1;
