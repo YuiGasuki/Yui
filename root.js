b102ab71b28890d8bff3e7e4803bbb1d8278137b
Yui <140684321+YuiGasuki@users.noreply.github.com>
2025-07-10 17:44:49 +0800

    Add files via upload

---
+++ root.js
@@ -0,0 +1,67 @@
+let pcpicture  = (Math.floor(Math.random() * (23 - 1 + 1)) + 1);
+let anpicture = (Math.floor(Math.random() * (30 - 1 + 1)) + 1);
+function replacepicture() {
+    if(window.innerHeight <= window.innerWidth){
+        document.getElementById('background_img').src = "p_" + pcpicture + ".webp";
+    }else{
+        document.getElementById('background_img').src = "b_" + anpicture + ".webp";
+    }
+}
+
+replacepicture();
+window.addEventListener('resize', replacepicture);
+const DarkMode = () =>{
+    if(document.cookie){
+        let data = document.cookie.split(";")[0].split("=")[1];
+        if(data==="true"){
+            document.documentElement.style.setProperty('--nightbackground', 'rgba(86,86,86,0.7)');
+            document.documentElement.style.setProperty('--nightbox', '0.5');           
+            document.documentElement.style.setProperty('--nightfcolor', '#aaaaaa');
+            return
+        }
+    }
+    document.documentElement.style.setProperty('--nightbackground', 'rgba(255,255,255,0.7)');
+    document.documentElement.style.setProperty('--nightbox', '0');
+    document.documentElement.style.setProperty('--nightfcolor', '#606266');
+    
+}
+
+DarkMode();
+const channel = new BroadcastChannel('Yui_night');
+channel.addEventListener('message', (e) => {
+DarkMode();
+})
+
+let getListCacheText = ['','',''];
+function getList(el,title,nu){
+    if(getListCacheText[nu]===''){
+    fetch(`https://api.github.com/repos/yuigasuki/Yui/contents/${title}`, {
+    method: 'GET'
+    }).then(response => response.json()).then(data => {
+    
+    for(let i=0;i<data.length;i++){
+    getListCacheText[nu] += `<li><a href="/Yui/${data[i].path}">${data[i].name}</a><div>${sizeTurn(data[i].size)}</div></li>`
+    }
+    el.parentElement.innerHTML += `<ul>${getListCacheText[nu]}</ul>`;
+    });
+    }else{
+    el.parentElement.innerHTML += `<ul>${getListCacheText[nu]}</ul>`;
+    }
+}
+
+function sizeTurn(number){
+let Text="";
+let a = Math.trunc(number/1024);
+if(a>=1){
+if(a>=1024){
+Text = (number/(1024*1024)).toFixed(2)+"MB"
+}else{
+Text = (number/1024).toFixed(2)+"KB"
+}
+}else{
+Text = number+"B"
+}
+return Text
+
+
+}
