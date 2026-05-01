d8e9318dd9b5c5955090e234a40423140683e05b
Yui <140684321+YuiGasuki@users.noreply.github.com>
2026-05-01 12:19:08 +0800

    Add files via upload

--- tool/index.js
+++ tool/index.js
@@ -1,50 +1,100 @@
 const imgBox = document.getElementById('img_box');
-let ImgList = [[],[],[]];
-let rn= 0;
-function changeList(){
-ImgList = [[],[],[]];
-let m = (Math.floor(Math.random() * (7 - 1 + 1)) + 1);
-let r = (Math.floor(Math.random() * (7 - 1 + 1)) + 1);
-rn = (Math.floor(Math.random() * (6 - 3 + 1)) + 3)
-imgBox.innerHTML="";
-let li=0;
-for(let i=0;i<3;i++){
-for(let bi=0;bi<3;bi++){
-if((Math.floor(Math.random() * (2 - 1 + 1)) + 1)===1&&li<rn){
-if(r==8){
-r=1;
-}
-ImgList[i].push(1)
-imgBox.innerHTML+=`<img src="r_${r}.png" onclick="Right(${i},${bi},this)" />`;
-r++
-li++;
-}else{
-if(m==8){
-m=1
-}
-ImgList[i].push(0)
-imgBox.innerHTML+=`<img src="m_${m}.png" onclick="changeList()" />`;
-m++
-}
-}
-}
+let ImgList = [
+    [],
+    [],
+    []
+];
+let rn = 0;
+let MaxR = 10;
+let MaxM = 11;
+let r, m;
+let starTime, endTime;
+let BodyImgText = "";
+
+function changeList() {
+    endTime = 0;
+    ImgList = [
+        [],
+        [],
+        []
+    ];
+    BodyImgText = "";
+    let rList = []
+    for (let i = 1; i <= MaxR; i++) {
+        rList.push({
+            "id": i,
+            "type": 0
+        })
+    }
+    for (let i = 1; i <= MaxM; i++) {
+        rList.push({
+            "id": i,
+            "type": 1
+        })
+    }
+    alllist = shuffle(rList).slice(0, 9)
+    alllist_end = []
+    imgBox.innerHTML = "";
+    let li = 0;
+    for (let i = 0; i < 3; i++) {
+        alllist_end.push(alllist.slice(3 * i, 3 * (i + 1)))
+    }
+    for (let i = 0; i < 3; i++) {
+        for (let li = 0; li < 3; li++) {
+            ImgList[i].push(alllist_end[i][li]["type"])
+            if (alllist_end[i][li]["type"] == 1) {
+                BodyImgText += `<img src="m_${alllist_end[i][li]["id"]}.webp" onclick="changeList()" />`;
+            } else {
+                BodyImgText += `<img src="r_${alllist_end[i][li]["id"]}.webp" onclick="Right(${i},${li},this)" />`;
+            }
+        }
+
+    }
+    imgBox.innerHTML = BodyImgText;
+    starTime = Date.parse(new Date());
+
 }
 changeList()
 
 
-function Right(i,bi,ul){
-ImgList[i][bi]=0;
-ul.style.opacity = 0;
+function Right(i, bi, ul) {
+    ul.style.opacity = 0;
+    ImgList[i][bi] = 1;
 }
 
-function verification(){
-for(let i=0;i<3;i++){
-for(let bi=0;bi<3;bi++){
-if(ImgList[i][bi]==1){
-alert(`人机はだめ`)
-return
+function verification() {
+    for (let i = 0; i < 3; i++) {
+        for (let bi = 0; bi < 3; bi++) {
+            if (ImgList[i][bi] == 0) {
+                alert(`验证失败`)
+                return
+            }
+        }
+    }
+    if (endTime === 0) {
+        endTime = new Date().getTime();
+    }
+
+
+    alert(`验证成功，用时${TimeTurn(endTime,starTime)}`)
 }
+
+function TimeTurn(time, star) {
+    let date = [new Date(time), new Date(star)]; // 参数需要毫秒数，所以这里将秒数乘于 1000
+    m = date[0].getMinutes() - date[1].getMinutes();
+    s = (date[0].getSeconds() + (m * 60)) - date[1].getSeconds() + '秒';
+    return s;
 }
-}
-alert("验证成功")
+
+
+
+function shuffle(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len - 1; i++) {
+        var idx = Math.floor(Math.random() * (len - i));
+        var temp = arr[idx];
+        arr[idx] = arr[len - i - 1];
+        arr[len - i - 1] = temp;
+    }
+    return arr;
 }
