81c5544b7e3fafbff8ac8ca2621fcb4fdea2ef3e
Yui <140684321+YuiGasuki@users.noreply.github.com>
2026-07-28 20:52:09 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -423,7 +423,7 @@ if(new Date().getHours()>22||new Date().getHours()<4){
 changePicture.onclick = () =>{
     changePicture.style.animationPlayState="running";
     changePicture.style.pointerEvents="none";
-    document.getElementById('background_img').style.filter="blur(50px)";
+    document.getElementById('background_img').style.filter="blur(50px) contrast(0%)";
     setTimeout(()=>{
     if(window.innerHeight <= window.innerWidth){
         pcpicture = (Math.floor(Math.random() * (pcpictureN - 1 + 1)) + 1);
@@ -437,7 +437,7 @@ changePicture.onclick = () =>{
     
     setTimeout(()=>{
         changePicture.style.animationPlayState="paused";
-        document.getElementById('background_img').style.filter="blur(10px)";
+        document.getElementById('background_img').style.filter="blur(10px) contrast(100%)";
         setTimeout(()=>{
             changePicture.style.pointerEvents="auto";
         },800);
@@ -494,7 +494,7 @@ const announcement = document.getElementById('announcement_p');
 let announcementData = `
 ! 本站是供站长(下文称"我")学习使用，图片素材来自网络，并非商用。无意侵犯贵方的利益请<a href="mailto:2528197707@qq.com">联系我</a>(<a href="mailto:l2528197707@gmail.com">备用邮箱</a>)，我将在48小时内进行删除。
 ☆ 开源地址<a href="https://github.com/YuiGasuki/Yui">Github</a> 本站为静态网站，仅供展示我的学习成果，静态托管在<a href="https://github.com/">Github</a>
-✎.本次更新内容: 修改了已知BUG
+✎.本次更新内容: 优化了背景切换
 `;
 
 /*
